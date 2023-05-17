0731d15fff4f13c07264f5efd5eef70562a82bd1
Alexander Kushnir <dj.egregor1@gmail.com>
2023-05-17 20:55:31 +0200

    Update pets.js

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -6,11 +6,19 @@ const validation = require('../../middlewares/validation');
 const authenticate = require('../../middlewares/authenticate');
 const addPetSchema = require('../../schemas/pets');
 
+const upload = require('../../middlewares/upload');
+
 const router = express();
 
 router.get('/', authenticate, ctrl.getUserPets);
 
-router.post('/', authenticate, validation.validate(addPetSchema), ctrl.addPet);
+router.post(
+    '/',
+    authenticate,
+    upload.single('photo'),
+    validation.validate(addPetSchema),
+    ctrl.addPet
+);
 
 router.delete('/:petId', authenticate, ctrl.removePet);
 
