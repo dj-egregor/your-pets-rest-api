28611816f76fc48ec29ee886e56c3d023cb014e4
Oleksandr <kulykof@gmail.com>
2023-05-17 22:32:25 +0300

    Update add pet and create user notice by category

--- controllers/pets/addPet.js
+++ controllers/pets/addPet.js
@@ -1,18 +1,46 @@
 const Pet = require('../../models/pet');
+const { NotFound } = require('http-errors');
+
+const path = require('path');
+const fs = require('fs/promises');
+const folder = 'pets-img';
+const petsImgDir = path.join(__dirname, '../', '../', 'public', folder);
+const resizeImage = require('../../utils/resizeImage/resizeImage');
 
 const addPet = async (req, res, next) => {
     try {
+        const { _id } = req.user;
+        const { path: tempUpload, originalname } = req.file;
+        const filename = `${_id}_${originalname}`;
+        const resultUpload = path.join(petsImgDir, filename);
+
+        const resizeResult = await resizeImage(tempUpload);
+        if (!resizeResult) {
+            throw new Error('Failed to resize image');
+        }
+
+        await fs.rename(tempUpload, resultUpload);
+        const imageURL = path.join(folder, filename);
+
         const { _id: owner } = req.user;
-        const pet = await Pet.create({ ...req.body, owner });
+        const { category } = req.params;
 
-        const result = pet.toObject();
-        delete result.createdAt;
-        delete result.updatedAt;
+        const pet = await Pet.create({
+            ...req.body,
+            owner,
+            category,
+            photoURL: imageURL,
+        });
 
-        res.status(201).json(result);
+        if (!pet) {
+            throw new NotFound('Not found');
+        }
+
+        res.status(201).json(pet);
     } catch (error) {
         next(error);
     }
 };
+
 
 module.exports = addPet;
