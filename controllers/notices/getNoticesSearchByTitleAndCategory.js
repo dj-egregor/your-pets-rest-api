cd0824b8bdb9d00b3e1fe45c107b6c35cd8f9422
Alexander Kushnir <dj.egregor1@gmail.com>
2023-05-24 13:32:48 +0200

    Update getNoticesSearchByTitleAndCategory.js

--- controllers/notices/getNoticesSearchByTitleAndCategory.js
+++ controllers/notices/getNoticesSearchByTitleAndCategory.js
@@ -1,11 +1,10 @@
 const { NotFound } = require('http-errors');
 const Notice = require('../../models/notice');
-// const User = require('../../models/user');
 
 const getNoticesSearchByTitleAndCategory = async (req, res, next) => {
     try {
         const { category } = req.params;
-        const { page = 1, limit = 12, q = '' } = req.query;
+        const { page = 1, limit = 12, q = '', age = [], sex } = req.query;
         const skip = (page - 1) * limit;
 
         console.log('req.params', req.params);
@@ -14,13 +13,63 @@ const getNoticesSearchByTitleAndCategory = async (req, res, next) => {
         const searchWords = q.trim().split(' ');
         const regex = new RegExp(searchWords, 'i');
 
-        const total = await Notice.countDocuments({
-            $and: [{ category }, { title: regex }],
-        });
+        const now = new Date();
 
-        const notices = await Notice.find({
-            $and: [{ category }, { title: regex }],
-        })
+        let ageRangeStart, ageRangeEnd;
+
+        if (age.includes('3-12m')) {
+            ageRangeStart = new Date(
+                now.getFullYear(),
+                now.getMonth() - 12,
+                now.getDate()
+            );
+            ageRangeEnd = new Date(
+                now.getFullYear(),
+                now.getMonth() - 3,
+                now.getDate()
+            );
+        } else if (age.includes('1year')) {
+            ageRangeStart = new Date(
+                now.getFullYear() - 2,
+                now.getMonth(),
+                now.getDate()
+            );
+            ageRangeEnd = new Date(
+                now.getFullYear() - 1,
+                now.getMonth(),
+                now.getDate()
+            );
+        } else if (age.includes('2year')) {
+            ageRangeStart = new Date(
+                now.getFullYear() - 3,
+                now.getMonth(),
+                now.getDate()
+            );
+            ageRangeEnd = new Date(
+                now.getFullYear() - 2,
+                now.getMonth(),
+                now.getDate()
+            );
+        }
+
+        console.log(ageRangeStart, ageRangeEnd);
+
+        const query = {
+            category,
+            title: regex,
+        };
+
+        if (sex) {
+            query.sex = Array.isArray(sex) ? { $in: sex } : sex;
+        }
+
+        if (ageRangeStart && ageRangeEnd) {
+            query.birthday = { $gte: ageRangeStart, $lt: ageRangeEnd };
+        }
+
+        const total = await Notice.countDocuments(query);
+
+        const notices = await Notice.find(query)
             .sort({ createdAt: -1 })
             .skip(skip)
             .limit(limit)
@@ -30,7 +79,7 @@ const getNoticesSearchByTitleAndCategory = async (req, res, next) => {
             });
 
         if (notices.length === 0) {
-            throw new NotFound(`There are no notices for this requestQQQQQQQQ`);
+            throw new NotFound(`There are no notices for this request`);
         }
 
         const totalPages = Math.ceil(total / limit);
